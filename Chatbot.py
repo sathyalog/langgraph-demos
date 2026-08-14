4f089958c2a7f55a62898e47ffb57f5cf7ef9b7b
Sathya Vakacharla <sathyavakacharla@Sathyas-MacBook-Air.local>
2026-08-14 15:39:16 +0100

    langgraph demo1 added

---
+++ Chatbot.py
@@ -0,0 +1,39 @@
+from langgraph.graph import START, END, StateGraph
+from langchain_openrouter import ChatOpenRouter
+from typing import TypedDict, Annotated
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenRouter(
+    model="gpt-4o-mini"
+)
+
+class ChatbotState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def ChatbotNode(state: ChatbotState) -> ChatbotState:
+    result = llm.invoke(state["messages"])
+    return {'messages': [result]}
+
+
+graph = StateGraph(ChatbotState)
+graph.add_node('ChatbotNode', ChatbotNode)
+graph.add_edge(START, 'ChatbotNode')
+graph.add_edge('ChatbotNode', END)
+
+workflow = graph.compile(checkpointer=InMemorySaver())
+
+# image = workflow.get_graph().draw_mermaid_png()
+# with open("chatbot_graph.png", mode="wb") as f:
+#     f.write(image)
+
+while True:
+    user_input=input("You: ")
+    if user_input.lower()=="exit":
+        break
+    response = workflow.invoke({'messages': HumanMessage(content=user_input)}, config={'configurable': {'thread_id': 1}})
+    print(response['messages'][-1].content)
